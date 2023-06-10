3655c79e3a690f8045933861cd177003639ccd92
ChiThang-50Cent <chithang.nydo@gmail.com>
2023-06-11 06:46:44 +0700

    update UI

--- src/pages/thaydoiquydinh/ThayDoiLoaiDaiLy.jsx
+++ src/pages/thaydoiquydinh/ThayDoiLoaiDaiLy.jsx
@@ -1,17 +1,138 @@
 import BackButton from "../../component/button/backbutton/BackButton";
+import React, { useState } from "react";
 
 function ThayDoiLoaiDaiLy() {
-   
+
+    const [agencies, setAgencies] = useState([
+        { id: 1, name: 'Đại lý A', maxDebt: 1000000 },
+        { id: 2, name: 'Đại lý B', maxDebt: 2000000 },
+        { id: 3, name: 'Đại lý C', maxDebt: 1500000 },
+    ]);
+
+    const [editingAgencyId, setEditingAgencyId] = useState(null);
+
+    const handleDelete = (id) => {
+        setAgencies(agencies.filter((agency) => agency.id !== id));
+    };
+
+    const handleEdit = (id) => {
+        setEditingAgencyId(id);
+    };
+
+    const handleSave = (id) => {
+        setEditingAgencyId(null);
+        // Lưu thông tin chỉnh sửa của đại lý
+        console.log(`Save changes for agency with ID ${id}`);
+    };
+
+    const handleAdd = () => {
+        const newAgencyId = agencies.length + 1;
+        setAgencies([
+            ...agencies,
+            { id: newAgencyId, name: '', maxDebt: 0 }
+        ]);
+        setEditingAgencyId(newAgencyId);
+    };
+
     return (
         <div>
             {/* Heading */}
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center mb-5">
                 <BackButton className="mr-4" />
                 <h2 className="mb-4 text-4xl font-bold text-center w-full">Danh sách loại đại lý</h2>
             </div>
             {/* Form */}
-            <div></div>
-                
+            <div>
+                <div className="flex flex-col items-start">
+                    <table className="w-1/2 mb-4">
+                        <thead>
+                            <tr>
+                                <th className="border px-4 py-2">Tên loại đại lý</th>
+                                <th className="border px-4 py-2">Số nợ tối đa</th>
+                                <th className="border px-4 py-2">Hành động</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {agencies.map((agency) => (
+                                <tr key={agency.id}>
+                                    <td className="border px-4 py-2">
+                                        {editingAgencyId === agency.id ? (
+                                            <input
+                                                type="text"
+                                                value={agency.name}
+                                                onChange={(e) =>
+                                                    setAgencies((prevAgencies) =>
+                                                        prevAgencies.map((prevAgency) =>
+                                                            prevAgency.id === agency.id
+                                                                ? { ...prevAgency, name: e.target.value }
+                                                                : prevAgency
+                                                        )
+                                                    )
+                                                }
+                                                className="border border-gray-300 p-2"
+                                            />
+                                        ) : (
+                                            agency.name
+                                        )}
+                                    </td>
+                                    <td className="border px-4 py-2">
+                                        {editingAgencyId === agency.id ? (
+                                            <input
+                                                type="number"
+                                                value={agency.maxDebt}
+                                                onChange={(e) =>
+                                                    setAgencies((prevAgencies) =>
+                                                        prevAgencies.map((prevAgency) =>
+                                                            prevAgency.id === agency.id
+                                                                ? { ...prevAgency, maxDebt: parseInt(e.target.value) }
+                                                                : prevAgency
+                                                        )
+                                                    )
+                                                }
+                                                className="border border-gray-300 p-2"
+                                            />
+                                        ) : (
+                                            agency.maxDebt
+                                        )}
+                                    </td>
+                                    <td className="border px-4 py-2">
+                                        {editingAgencyId === agency.id ? (
+                                            <button
+                                                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                                                onClick={() => handleSave(agency.id)}
+                                            >
+                                                Lưu
+                                            </button>
+                                        ) : (
+                                            <>
+                                                <button
+                                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+                                                    onClick={() => handleEdit(agency.id)}
+                                                >
+                                                    Sửa
+                                                </button>
+                                                <button
+                                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                                    onClick={() => handleDelete(agency.id)}
+                                                >
+                                                    Xóa
+                                                </button>
+
+                                            </>
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <button
+                            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={handleAdd}
+                        >
+                            Thêm loại đại lý
+                        </button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/thaydoiquydinh/ThayDoiThamSo.jsx
+++ src/pages/thaydoiquydinh/ThayDoiThamSo.jsx
@@ -1,17 +1,73 @@
 import BackButton from "../../component/button/backbutton/BackButton";
+import React, {useState} from "react";
 
 function ThayDoiThamSo() {
-   
+    const [maxAgents, setMaxAgents] = useState('');
+    const [allowOverdue, setAllowOverdue] = useState('true');
+    const [unitPrice, setUnitPrice] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Xử lý logic khi submit form
+        console.log('Max Agents:', maxAgents);
+        console.log('Allow Overdue:', allowOverdue);
+        console.log('Unit Price:', unitPrice);
+
+        // Reset giá trị của các input sau khi submit
+        setMaxAgents('');
+        setAllowOverdue('true');
+        setUnitPrice('');
+    };
     return (
         <div>
             {/* Heading */}
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center mb-5">
                 <BackButton className="mr-4" />
                 <h2 className="mb-4 text-4xl font-bold text-center w-full">Danh sách tham số</h2>
             </div>
             {/* Form */}
-            <div></div>
-                
+            <div className="w-1/2 ml-0">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4 flex items-center">
+                        <label className="w-auto mr-3">Số đại lý tối đa trong một quận:</label>
+                        <input
+                            type="number"
+                            id="max-agents"
+                            className="border border-gray-300 p-2 flex-grow"
+                            value={maxAgents}
+                            onChange={(e) => setMaxAgents(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-4 flex items-center">
+                        <label className="w-auto mr-3">Cho phép thu vượt tiền nợ:</label>
+                        <select
+                            id="allow-overdue"
+                            className="border border-gray-300 p-2 flex-grow"
+                            value={allowOverdue}
+                            onChange={(e) => setAllowOverdue(e.target.value)}
+                        >
+                            <option value="true">True</option>
+                            <option value="false">False</option>
+                        </select>
+                    </div>
+                    <div className="mb-4 flex items-center">
+                        <label className="w-auto mr-3">Tỷ lệ đơn giá xuất:</label>
+                        <input
+                            type="number"
+                            id="unit-price"
+                            className="border border-gray-300 p-2 flex-grow"
+                            value={unitPrice}
+                            onChange={(e) => setUnitPrice(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex justify-end">
+                        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
