b031db46df496f23fe49e0d7c9cfc328ea4b5669
tannnt23 <nnthientan07@gmail.com>
2023-06-11 17:00:58 +0700

    Create DangKyDaiLy, add mutation

--- src/pages/dangkydaily/DangKyDaiLy.jsx
+++ src/pages/dangkydaily/DangKyDaiLy.jsx
@@ -1,10 +1,181 @@
+import React, { useState, useEffect } from 'react';
+import { useMutation, useQuery } from '@apollo/client';
+import { queryEveryQuan, queryEveryLoaidaily } from '../../graphql/queries';
+import { addDailyMutation } from '../../graphql/mutations';
 
-import React from 'react'
 
-function DangKyDaiLy() {
+
+const AddDailyForm = () => {
+
+  // Define the mutation
+  const [addDaily] = useMutation(addDailyMutation);
+
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Extract the values from the form
+    const MaLoaiDaiLy = selectedLoaiDaiLy;
+    const MaQuan = selectedQuan;
+    const DienThoai = dienThoai;
+    const TenDaiLy = tenDaiLy;
+    const DiaChi = diaChi;
+    console.log(MaLoaiDaiLy, MaQuan, DienThoai, TenDaiLy, DiaChi)
+    console.log(typeof DienThoai)
+
+    try {
+      // Perform the mutation
+      const { data } = await addDaily({
+        variables: {
+          MaLoaiDaiLy,
+          MaQuan,
+          DienThoai,
+          TenDaiLy,
+          DiaChi
+        },
+      });
+      console.log(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const [selectedLoaiDaiLy, setSelectedLoaiDaiLy] = useState('');
+  const [selectedQuan, setSelectedQuan] = useState('');
+  const [tenDaiLy, setTenDaiLy] = useState('');
+  const [dienThoai, setDienThoai] = useState('');
+  const [diaChi, setDiaChi] = useState('');
+
+  const { loading: loadingQuan, error: errorQuan, data: dataQuan } = useQuery(queryEveryQuan);
+  const { loading: loadingLoaiDaiLy, error: errorLoaiDaiLy, data: dataLoaiDaiLy } = useQuery(queryEveryLoaidaily);
+  useEffect(() => {
+    console.log(selectedLoaiDaiLy); // Log the updated value of selectedDaily
+  }, [selectedLoaiDaiLy]);
+
+  if (loadingLoaiDaiLy) {
+    return <div>Loading...</div>;
+  }
+
+  if (errorLoaiDaiLy) {
+    return <div>Error fetching dailyData</div>;
+  }
+  // const [addDaily] = useMutation(addDailyMutation);
+
+  const handleLoaiDaiLySelectionChange = (event) => {
+    setSelectedLoaiDaiLy(event.target.value);
+  };
+  const handleQuan = (event) => {
+    setSelectedQuan(event.target.value);
+  };
+  const handleTenDaiLy = (event) => {
+    setTenDaiLy(event.target.value);
+  };
+  const handleDienThoai = (event) => {
+    setDienThoai(event.target.value);
+  };
+  const handleDiaChi = (event) => {
+    setDiaChi(event.target.value);
+  };
+
+  if (loadingQuan) return <p>Loading...</p>;
+  if (errorQuan) return <p>Error fetching data</p>;
+
   return (
-    <div>DangKyDaiLy</div>
-  )
-}
+    <form onSubmit={handleSubmit} className="space-y-4 w-1/2">
+      <div>
+        <label htmlFor="TenDaiLy" className="block font-medium">
+          Tên đại lý
+        </label>
+        <input
+          type="text"
+          id="TenDaiLy"
+          name="TenDaiLy"
+          value={tenDaiLy}
+          onChange={handleTenDaiLy}
+          className="w-full p-2 border border-gray-600 rounded-md"
+          placeholder='Nhập tên đại lý...'
+        />
+      </div>
+      <div>
+        <label htmlFor="DienThoai" className="block font-medium">
+          Số điện thoại
+        </label>
+        <input
+          type="text"
+          id="DienThoai"
+          name="DienThoai"
+          value={dienThoai}
+          onChange={handleDienThoai}
+          className="w-full p-2 border border-gray-600 rounded-md"
+          placeholder='Nhập số điện thoại...'
+        />
+      </div>
+      <div>
+        <label htmlFor="DiaChi" className="block font-medium">
+          Địa chỉ
+        </label>
+        <input
+          type="text"
+          id="DiaChi"
+          name="DiaChi"
+          value={diaChi}
+          onChange={handleDiaChi}
+          className="w-full p-2 border border-gray-600 rounded-md"
+          placeholder='Nhập địa chỉ...'
+        />
+      </div>
 
-export default DangKyDaiLy
+      <div>
+        <label htmlFor="MaQuan" className="block font-medium">
+          Mã Quận
+        </label>
+        <select
+          id="MaQuan"
+          name="MaQuan"
+          value={selectedQuan}
+          onChange={handleQuan}
+          className="w-full p-2 border border-gray-600 rounded-md"
+        >
+          <option value="">Chọn mã quận...</option>
+          {dataQuan.everyQuan.map((quan) => (
+            <option key={quan.MaQuan} value={quan.MaQuan}>
+              {quan.MaQuan}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="MaLoaiDaiLy" className="block font-medium">
+          Mã loại đại lý
+        </label>
+        <select
+          id="MaLoaiDaiLy"
+          name="MaLoaiDaiLy"
+          value={selectedLoaiDaiLy}
+          onChange={handleLoaiDaiLySelectionChange}
+          className="w-full p-2 border border-gray-600 rounded-md"
+        >
+          <option value="">Chọn mã loại đại lý...</option>
+          {dataLoaiDaiLy.everyLoaidaily.map((loai) => (
+            <option key={loai.MaLoaiDaiLy} value={loai.MaLoaiDaiLy}>
+              {loai.MaLoaiDaiLy}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Repeat the above structure for the remaining fields */}
+
+      <button
+        type="submit"
+        onClick={handleSubmit}
+        className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default AddDailyForm;
--- src/pages/lapphieuthutien/LapPhieuThuTien.jsx
+++ src/pages/lapphieuthutien/LapPhieuThuTien.jsx
@@ -2,7 +2,7 @@ import BackButton from "../../component/button/backbutton/BackButton";
 
 import { useQuery, useMutation } from '@apollo/client';
 import { queryEveryDaily } from '../../graphql/queries';
-import {updateDailyMutation, addPhieuthutienMutation} from '../../graphql/mutations'
+import { updateDailyMutation, addPhieuthutienMutation } from '../../graphql/mutations'
 import { useState, useEffect } from 'react';
 
 function LapPhieuThuTien() {
@@ -35,7 +35,7 @@ function LapPhieuThuTien() {
         let option = 0
         let choosen = {}
 
-        
+
         option = parseInt(daiLyDuocChon.split(' ')[1])
         choosen = daiLy[option]
 
@@ -54,48 +54,48 @@ function LapPhieuThuTien() {
                 <BackButton className="mr-4" />
                 <h2 className="mb-4 text-4xl font-bold text-center w-full">Phiếu Thu Tiền</h2>
             </div>
-        
-//             <div>
-//                 <form className="p-4">
-//                     <div className="flex flex-col w-1/3">
-//                         <div className="">
-//                             <label htmlFor="combo-box" className="block mb-2">Tên đại lý</label>
-//                             <select id="combo-box" name="combo-box" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-//                                 value={daiLyDuocChon}
-//                                 onChange={handleChonDaiLy}
-//                             >
-//                                 {daiLy.map((item, index) => (
-//                                     <option key={index} value={`option ${index}`}>{item.TenDaiLy}</option>
-//                                 ))}
-//                             </select>
-//                         </div>
-//                         <div className="">
-//                             <label htmlFor="date-input" className="block mt-4 mb-2">Ngày lập phiếu</label>
-//                             <input type="date" id="date-input" name="date-input" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
-//                         </div>
-//                     </div>
-//                     <br />
-//                     <br />
-//                     <div className="w-2/3 ">
-//                         <input className='w-96 border border-gray-500 rounded-lg p-2 mb-2' type="number" placeholder="Nhập số tiền (VNĐ)" value={thuTien} onChange={(e) => setThuTien(e.target.value)} />
-//                     </div>
-//                     <div className="flex items-center">
-//                         <button onClick={handleShowNo} className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-green-700 ease-in-out transition duration-300 cursor-pointer">
-//                             Xem nợ</button>
-//                         {showNo && (
-//                             <div className="mx-4 px-4 py-2">
-//                                 {tienNo}
-//                             </div>
-//                         )}
-//                     </div>
-//                     <div className="flex justify-end w-4/5 ">
-//                         <input type="submit" value="Submit" className="mt-4 px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-green-700 ease-in-out transition duration-300 cursor-pointer" />
-//                     </div>
+
+            <div>
+                <form className="p-4">
+                    <div className="flex flex-col w-1/3">
+                        <div className="">
+                            <label htmlFor="combo-box" className="block mb-2">Tên đại lý</label>
+                            <select id="combo-box" name="combo-box" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                                value={daiLyDuocChon}
+                                onChange={handleChonDaiLy}
+                            >
+                                {daiLy.map((item, index) => (
+                                    <option key={index} value={`option ${index}`}>{item.TenDaiLy}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="">
+                            <label htmlFor="date-input" className="block mt-4 mb-2">Ngày lập phiếu</label>
+                            <input type="date" id="date-input" name="date-input" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                    <div className="w-2/3 ">
+                        <input className='w-96 border border-gray-500 rounded-lg p-2 mb-2' type="number" placeholder="Nhập số tiền (VNĐ)" value={thuTien} onChange={(e) => setThuTien(e.target.value)} />
+                    </div>
+                    <div className="flex items-center">
+                        <button onClick={handleShowNo} className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-green-700 ease-in-out transition duration-300 cursor-pointer">
+                            Xem nợ</button>
+                        {showNo && (
+                            <div className="mx-4 px-4 py-2">
+                                {tienNo}
+                            </div>
+                        )}
+                    </div>
+                    <div className="flex justify-end w-4/5 ">
+                        <input type="submit" value="Submit" className="mt-4 px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-green-700 ease-in-out transition duration-300 cursor-pointer" />
+                    </div>
 
                 </form>
             </div> */}
 
-//         </div>
+        </div>
     )
 }
 
